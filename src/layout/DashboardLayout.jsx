a360dc101409a3627d96971ef1612d3f35d3d8e2
Khurshidbek <xurshidbekoktamaliyev651@gmail.com>
2025-04-11 18:14:08 +0500

    fix

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Outlet } from "react-router";
 import Sidebar from "./Sidebar";
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 
 function DashboardLayout() {
     return (
